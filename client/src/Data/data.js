42216929b13797241db788d8bad1d187d80643d8
Aryan Gupta <108828389+aryan10744@users.noreply.github.com>
2023-03-01 18:29:08 +0530

    Layout setup
    Layout setup + a little more

---
+++ client/src/Data/data.js
@@ -0,0 +1,28 @@
+export const SidebarMenu = [
+    {
+        name:'Home',
+        path:'/Home',
+        icon: "fa-solid fa-house",
+    },
+    {
+        name:'Appointments',
+        path: "/appointments",
+        icon: 'fa-solid fa-list',
+    },
+    {
+        name:'Apply Doctor',
+        path:'/apply-doctor',
+        icon:"fa-duotone fa-user-tie",
+    },
+    {
+        name:'Profile',
+        path:'/profile',
+        icon:"fa-solid fa-user",
+    },
+    {
+        name:'Logout',
+        path:"/logout",
+        icon:"fa-solid fa-right-from-bracket",
+    },
+
+]
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import Layout from "./../components/Layout";
 const Homepage = () =>{
     const getUserData = async() => 
     {
@@ -22,25 +23,9 @@ const Homepage = () =>{
     }, []);
 
     return (
-        <React.Fragment>
-            <h1> Appointment Scheduler BPGC</h1>
-            <div className="container">
-               <div className="row">
-                <div className="col-sm">
-                <div class="card">
-  <img src={"images\BPGClogo.png"} class="card-img-top" alt="..." />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">
-    <a href="\login" className="btn btn-primary" >Login</a>
-    <a href="\register" className="btn btn-primary">Register</a>
-    </p>
-  </div>
-</div>.
-                </div>
-            </div>
-            </div>
-        </React.Fragment>
+            <Layout>
+                <h1>Home page </h1>
+            </Layout>
     );
 };
 
